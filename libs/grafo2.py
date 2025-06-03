b8c704acaab45b2a71219dd15fa77f7b3de0a165
Luciano Almeida <luciano.e.almeida@gmail.com>
2025-06-03 12:23:36 -0300

    refactor: faster algorithm + dataset

--- libs/grafo2.py
+++ libs/grafo2.py
@@ -1,14 +1,11 @@
 """ Thanks vns-sop: https://github.com/ctu-mrs/vns-sop/blob/master/visualization/show_solution.py
 """
-import math
 
 import alphashape as alphashape
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 import numpy as np
-#from descartes import PolygonPatch
-from matplotlib.patches import Polygon as mplPolygon
-from shapely.geometry import MultiPoint
+from descartes import PolygonPatch
 
 
 class COPS:
@@ -56,7 +53,6 @@ class COPS:
             for j in range(i+1, self.dimension):
                 a = np.array(self.list_vertex[i])
                 b = np.array(self.list_vertex[j])
-                #dist = math.ceil(np.linalg.norm(a-b))
                 dist = np.linalg.norm(a-b)
                 self.matrix_dist[i][j] = dist
                 self.matrix_dist[j][i] = dist
@@ -68,7 +64,6 @@ class COPS:
             self.draw_3D(path, legend, fill_cluster, fill_set, name, save_img)
         else:
             self.draw_2D(path, legend, fill_cluster, fill_set, name, save_img)
-            pass
 
     def draw_2D(self, path=[], legend=[], fill_cluster=False, fill_set=False, name='saved2D', save_img=False):
         sets = self.list_clusters
@@ -101,20 +96,14 @@ class COPS:
             mark_index = 1
             black = "#080c0f"
             ax.scatter(xdata, ydata, color=black, label=label, marker=mark[mark_index], alpha=0.8, zorder=2)
-            '''if fill_cluster:
+            if fill_cluster:
                 ppp = ([[points[p][0], points[p][1]] for p in clusters[c]])
                 concave_hull = alphashape.alphashape(ppp, 0.)
                 dilated = concave_hull.buffer(dilate_size * 0.6)
                 patch1 = PolygonPatch(dilated, fc=this_color, ec=this_color, alpha=0.9, zorder=1)
-                ax.add_patch(patch1)'''
-            if fill_cluster:
-                cluster_points = MultiPoint([(points[p][0], points[p][1]) for p in clusters[c]])
-                concave_hull = cluster_points.convex_hull
-                dilated = concave_hull.buffer(dilate_size)
-                mpl_patch = mplPolygon(dilated.exterior.coords, fc=this_color, ec=this_color, alpha=0.1, zorder=1)
-                ax.add_patch(mpl_patch)
+                ax.add_patch(patch1)
 
-        '''if fill_set:
+        if fill_set:
             for s in range(len(sets)):
                 ppp = ([[points[p][0], points[p][1]] for c in sets[s] for p in clusters[c]])
                 concave_hull = alphashape.alphashape(ppp, 0.0)
@@ -122,14 +111,7 @@ class COPS:
                 black = "#000000"
                 white = "#f5f6f7"
                 patch1 = PolygonPatch(dilated, fc=white, ec=black, linewidth=2, alpha=0.3, zorder=0)
-                ax.add_patch(patch1)'''
-        if fill_set:
-            for s in range(len(sets)):
-                combined_points = MultiPoint([(points[p][0], points[p][1]) for c in sets[s] for p in clusters[c]])
-                concave_hull = combined_points.convex_hull
-                dilated = concave_hull.buffer(dilate_size * 1.8)
-                mpl_patch = mplPolygon(dilated.exterior.coords, fc='white', ec='black', linewidth=2, alpha=0.3, zorder=0)
-                ax.add_patch(mpl_patch)
+                ax.add_patch(patch1)
 
         for segment in path:
             x = [points[segment[0]][0], points[segment[1]][0]]
@@ -170,7 +152,6 @@ class COPS:
         #fig.set_size_inches(10, 5)#(10.5, 18.5)
         if save_img:
             plt.savefig(name)
-        plt.grid()
         plt.show()
         plt.close('all')
 
@@ -204,18 +185,12 @@ class COPS:
             #mark_index = c % len(mark)
             mark_index = 1
             ax.scatter(xdata, ydata, zdata, color=this_color, label=label, marker=mark[mark_index], alpha=0.8, zorder=2)
-            '''if fill_cluster:
+            if fill_cluster:
                 ppp = ([[points[p][0], points[p][1]] for p in clusters[c]])
                 concave_hull = alphashape.alphashape(ppp, 0.)
                 dilated = concave_hull.buffer(dilate_size * 0.6)
                 patch1 = PolygonPatch(dilated, fc=this_color, ec=this_color, alpha=0.9, zorder=1)
-                ax.add_patch(patch1)'''
-            if fill_cluster:
-                cluster_points = MultiPoint([(points[p][0], points[p][1]) for p in clusters[c]])
-                concave_hull = cluster_points.convex_hull
-                dilated = concave_hull.buffer(dilate_size * 0.6)
-                mpl_patch = mplPolygon(dilated.exterior.coords, fc=this_color, ec=this_color, alpha=0.9, zorder=1)
-                ax.add_patch(mpl_patch)
+                ax.add_patch(patch1)
 
         if fill_set:
             for s in range(len(sets)):
@@ -354,30 +329,3 @@ class COPS:
             total += dist
             print(f"{p} - {dist}")
             print(f"Total walked {total}")
-
-
-if __name__ == '__main__':
-    import os
-    cops = COPS()
-    dir = fr"../datasets/cop_cops_dataset"
-    problem = "eil51s25g1q2"
-    dataset = fr"{dir}\{problem}.cops"
-    cops.read_data(dataset)
-    selected_vertex = [0,10,50,46,3,11,4,48,9,29,8,49,28,1,2,27,7,6,47,5,26,0]
-    route = [0,10,50,46,3,11,4,48,9,29,8,49,28,1,2,27,7,6,47,5,26,0]
-    solution = {"profit": 0,
-                 "distance": 0,
-                 "route": [(0,10),(10,50),(50,46),(46,3),(3,11),(11,4),(4,48),(48,9),(9,29),(29,8),(8,49),(49,28),(28,1),(1,2),(2,27),(27,7),(7,6),(6,47),(47,5),(5,26),(26,0)],
-                 "subgroups_visited": [],
-                 # "sets_visited": [],
-                 }
-
-    from libs.tsp import two_opt, path_distance_for_circular, path_distance_non_circular
-    #distance = path_distance_for_circular(route, selected_vertex)  # only for conference
-
-    save_img = True
-    legend = ["r"]
-    img_saved = fr"{dir}/img/{problem}"
-    cops.draw(path=solution["route"], legend=legend, fill_cluster=True, fill_set=True, name=img_saved,
-              save_img=save_img)
-
